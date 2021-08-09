0dbbbd779ce117f3c815f7bd6b13da77afabb104
RiGun-K <1234suho@naver.com>
2021-08-09 16:50:38 +0900

    Main Page Edit

--- app/src/main/java/com/example/teamprojectt/MainActivity.java
+++ app/src/main/java/com/example/teamprojectt/MainActivity.java
@@ -1,4 +1,89 @@
 package com.example.teamprojectt;
 
-public class MainActivity {
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView; // 바텀 네비게이션 뷰 (하단바)
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+
+    private HomeFragment HomeFragment;
+    private DashboardFragment DashboardFragment;
+    private NotificationsFragment NotificationsFragment;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {    // 액티비티 시작시 처음으로 실행
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // 객체들을 생성
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+
+        // 프래그먼트를 트랜잭션 작업
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_home:
+                        setFrag(0);
+                        break;
+                    case R.id.navigation_dashboard:
+                        setFrag(1);
+                        break;
+                    case R.id.navigation_notifications:
+                        setFrag(2);
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+        HomeFragment = new HomeFragment();
+        DashboardFragment = new DashboardFragment();
+        NotificationsFragment = new NotificationsFragment();
+
+        // 처음 진입할시 (첫화면) 설정
+        setFrag(0);   // case 0 번째에 해당하는 화면으로
+
+
+    }
+
+    // 프래그먼트 교체가 일어나는 실행문
+    private void setFrag(int n) {
+        fm = getSupportFragmentManager();
+        ft = fm.beginTransaction();
+        // 프레그먼트 교체시에 이 프레그먼트를 가져와서 트렌잭션을 하겠다.
+        switch (n) {
+            case 0:
+                ft.replace(R.id.main_frame, HomeFragment);
+                ft.commit(); // commit = 저장
+                break;
+            case 1:
+                ft.replace(R.id.main_frame, DashboardFragment);
+                ft.commit();
+                break;
+            case 2:
+                ft.replace(R.id.main_frame, NotificationsFragment);
+                ft.commit();
+                break;
+        }
+
+
+    }
+
+
 }
--- app/src/main/java/com/example/teamprojectt/NotificationsFragment.java
+++ app/src/main/java/com/example/teamprojectt/NotificationsFragment.java
@@ -1,64 +1,31 @@
 package com.example.teamprojectt;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link NotificationsFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.jetbrains.annotations.NotNull;
+
 public class NotificationsFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private View view;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public NotificationsFragment() {
-        // Required empty public constructor
-    }
+    // Ctrl + o
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment NotificationsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static NotificationsFragment newInstance(String param1, String param2) {
-        NotificationsFragment fragment = new NotificationsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
+    @Nullable
+    @org.jetbrains.annotations.Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_notifications,container,false);
+        // home layout 과 연결
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notifications2, container, false);
+        return view;
     }
 }
