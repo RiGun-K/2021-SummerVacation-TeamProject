fc8747b54b12b2d7add457afb1a8bbb3e84385b5
RiGun-K <1234suho@naver.com>
2021-08-17 19:32:12 +0900

    My Page

--- app/src/main/java/com/example/teamprojectt/NotificationsFragment.java
+++ app/src/main/java/com/example/teamprojectt/NotificationsFragment.java
@@ -1,9 +1,11 @@
 package com.example.teamprojectt;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,7 +16,7 @@ import org.jetbrains.annotations.NotNull;
 public class NotificationsFragment extends Fragment {
 
     private View view;
-
+    private Button btn_edit;
 
     // Ctrl + o
 
@@ -25,6 +27,19 @@ public class NotificationsFragment extends Fragment {
     public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_notifications,container,false);
         // home layout 과 연결
+
+
+        btn_edit = view.findViewById(R.id.DashboardButton);
+        // 해당 xml 안에 있는 Button 객체 생성
+
+        btn_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), DashboardActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
         return view;
     }
