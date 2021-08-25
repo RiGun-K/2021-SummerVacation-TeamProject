32b38e2056bd38a7c31926049b1ead130f86ea43
RiGun-K <1234suho@naver.com>
2021-08-25 20:38:43 +0900

    2021/08/25

--- app/src/main/java/com/example/teamprojectt/HomeActivity2.java
+++ app/src/main/java/com/example/teamprojectt/HomeActivity2.java
@@ -4,14 +4,77 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class HomeActivity2 extends AppCompatActivity {
+
+    private static String IP_ADDRESS = "http://su1318ho.dothome.co.kr/"
+    private static String TAG = "phptest";
+
+    private EditText mEditTextName;
+    private EditText mEditTextCountry;
+    private TextView mTextViewResult;
+    private ArrayList<PersonalData> mArrayList;
+    private UsersAdapter mAdapter;
+    private RecyclerView mRecyclerView;
+    private EditText mEditTextSearchKeyword;
+    private String mJsonString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mypage2);
 
+
+        mTextViewResult = (TextView)findViewById(R.id.textView_main_result);
+        mRecyclerView = (RecyclerView) findViewById(R.id.listView_main_list);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mTextViewResult.setMovementMethod(new ScrollingMovementMethod());
+
+
+
+        mArrayList = new ArrayList<>();
+
+        mAdapter = new UsersAdapter(this, mArrayList);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+        Button button_all = (Button) findViewById(R.id.button_main_all);
+        button_all.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                mArrayList.clear();
+                mAdapter.notifyDataSetChanged();
+
+                GetData task = new GetData();
+                task.execute( "http://" + IP_ADDRESS + "/getjson.php", "");
+            }
+        });
     }
 
 }
