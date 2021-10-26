59bf83438ddce1ea80e2b53b5896275cb4ef2d78
RiGun-K <1234suho@naver.com>
2021-10-27 03:25:59 +0900

    RealTime Firebase + Chat

--- app/src/main/java/com/example/teamprojectt/SnsActivity.java
+++ app/src/main/java/com/example/teamprojectt/SnsActivity.java
@@ -1,30 +1,25 @@
 package com.example.teamprojectt;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.database.annotations.Nullable;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
-import java.net.HttpCookie;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
