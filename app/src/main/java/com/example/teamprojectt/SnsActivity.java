aa89abfaf83bd988da7cef521cd6b739732672cd
RiGun-K <1234suho@naver.com>
2021-10-26 19:56:46 +0900

    RealTime Firebase

--- app/src/main/java/com/example/teamprojectt/SnsActivity.java
+++ app/src/main/java/com/example/teamprojectt/SnsActivity.java
@@ -1,10 +1,20 @@
 package com.example.teamprojectt;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SnsActivity extends AppCompatActivity {
+
+    // 처음 시작은 Firebase console 에서
+    // [프로젝트 만들기 or 추가] 를 통해 작업 순서[Workflow] 대로
+    // 이 프로젝트와 firebase 연결
+
+    TextView tv;
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +22,11 @@ public class SnsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sns);
 
         // Sns 댓글창
+        tv = findViewById(R.id.tv);
+        et = findViewById(R.id.et);
+    }
+
+    public void clickSave(View view) {
 
     }
 }
