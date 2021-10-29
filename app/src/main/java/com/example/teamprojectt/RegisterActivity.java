858fcc64c04b1b5a4b18511721f61164c567744d
RiGun-K <1234suho@naver.com>
2021-10-29 17:40:27 +0900

    RealTime Firebase + Chat

--- app/src/main/java/com/example/teamprojectt/RegisterActivity.java
+++ app/src/main/java/com/example/teamprojectt/RegisterActivity.java
@@ -40,7 +40,7 @@ public class RegisterActivity extends AppCompatActivity {
             et_pass = findViewById(R.id.et_pass);
             et_name = findViewById(R.id.et_name);
             et_hakbun = findViewById(R.id.et_hakbun);
-            et_photo = findViewById(R.id.et_photo);
+//            et_photo = findViewById(R.id.et_photo);
 
             /////////////////////// 중복 체크  하다 말앗음 php 없어서
             final Button validateButton = (Button) findViewById(R.id.validateButton);
